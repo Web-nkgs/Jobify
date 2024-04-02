c8591c16c8fe43ad1cc69ca37e76117c2376eb0e
neddyKG <gonzalezneddy@gmail.com>
2024-04-02 11:51:43 -0400

    Created basic Job CRUD

---
+++ controllers/jobController.js
@@ -0,0 +1,73 @@
+import { nanoid } from 'nanoid'
+
+let jobs = [
+    {id: nanoid(), company: 'apple', position: 'front-end'},
+    {id: nanoid(), company: 'google', position: 'back-end'}
+]
+
+export const getAllJobs = async (req, res) => {
+    res.status(200).json({jobs})
+}
+
+export const createJob = async (req, res) => {
+    const { company, position } = req.body
+    if (!company || !position) {
+        res.status(400).json({message: "Missing values"})
+        return 
+    }
+
+    const job = {
+        id: nanoid(10),
+        company,
+        position
+    }
+
+    jobs.push(job)
+    res.status(201).json({job})
+}
+
+export const getJob = async (req, res) => {
+    const { id } = req.params
+    const job = jobs.find((job) => job.id === id)
+
+    if (!job) {
+        throw new Error('No job with that id')
+        // res.status(404).json({message: `No job found with id: ${id}`})
+        // return
+    }
+
+    res.status(200).json({job})
+}
+
+export const updateJob = async (req, res) => {
+    const { company, position } = req.body
+    if (!company || !position) {
+        res.status(404).json({message: 'Missing values'})
+        return
+    }
+
+    const { id } = req.params
+    let job = jobs.find(job => job.id === id)
+    if (!job) {
+        res.status(404).json({message: `No job found with id: ${id}`})
+        return
+    }
+
+    job.company = company
+    job.position = position
+    res.status(200).json({ message: "job modified", job })
+}
+
+export const deleteJob = async (req, res) => {
+    const { id } = req.params
+    const job = jobs.find(job => job.id === id)
+
+    if (!job) {
+        res.status(404).json({ message: `No job found with id: ${id}` })
+        return
+    }
+
+    jobs = jobs.filter(job => job.id !== id)
+
+    res.status(200).json({ message: "Job deleted successfully!" })
+}
---
+++ server.js
@@ -0,0 +1,46 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+import morgan from 'morgan'
+import { nanoid } from 'nanoid'
+import jobRouter from './routes/jobRouter.js'
+
+const app = express()
+
+// Middleware --> app.use
+app.use(express.json())
+
+let jobs = [
+    {id: nanoid(), company: 'apple', position: 'front-end'},
+    {id: nanoid(), company: 'google', position: 'back-end'}
+]
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+// app.post('/', (req, res) => {
+//     console.log(req)
+//     res.json({ message: 'data received', data: req.body })
+// })
+
+app.use('/api/v1/jobs', jobRouter)
+
+app.use('*', (req, res) => {
+    res.status(404).json({message: 'Not found'})
+})
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).json({ message: 'something went wrong'})
+})
+
+const port = process.env.PORT || 5100
+
+app.listen(port, () => {
+    console.log(`server running on port ${port}...`)
+})
